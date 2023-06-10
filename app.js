0164874c10686727776a786a634a686a2f346c62
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2023-06-10 12:01:33 +0530

    add booking

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const collection = require("./mongo");
-const routes = require("./routes/routes");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const collection = require('./mongo');
+const routes = require('./routes/routes');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/', cors(), (req, res) => {});
+app.get('/consultant', cors(), (req, res) => {});
 
-app.get("/", cors(), (req, res) => {});
-
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
   const { email, password } = req.body;
 
   try {
@@ -21,16 +21,59 @@ app.post("/", async (req, res) => {
     });
 
     if (check) {
-      res.json("exist");
+      res.json('exist');
     } else {
-      res.json("notexist");
+      res.json('notexist');
     }
   } catch (e) {
-    res.json("fail");
+    res.json('fail');
+  }
+});
+app.post('/consultant', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  try {
+    const check = await Consultant.findOne({
+      email: email,
+      password: password,
+    });
+
+    if (check) {
+      res.json('exist');
+    } else {
+      res.json('notexist');
+    }
+  } catch (e) {
+    res.json('fail');
+  }
+});
+app.post('/registerc', async (req, res) => {
+  const { name, email, speciality, password, age } = req.body;
+
+  const data = {
+    name: name,
+    email: email,
+    speciality: speciality,
+    password: password,
+    age: age,
+    isApproved: false,
+  };
+
+  try {
+    const check = await Consultant.findOne({ email: email });
+
+    if (check) {
+      res.json('exist');
+    } else {
+      await Consultant.insertMany([data]);
+      res.json('notexist');
+    }
+  } catch (e) {
+    res.json('fail');
   }
 });
 
-app.post("/register", async (req, res) => {
+app.post('/register', async (req, res) => {
   const { name, email, city, college, password } = req.body;
 
   const data = {
@@ -45,39 +88,36 @@ app.post("/register", async (req, res) => {
     const check = await collection.findOne({ email: email });
 
     if (check) {
-      res.json("exist");
+      res.json('exist');
     } else {
-      res.json("notexist");
+      res.json('notexist');
       await collection.insertMany([data]);
     }
   } catch (e) {
-    res.json("fail");
+    res.json('fail');
   }
 });
 
-app.use("/api/routes", routes);
+app.use('/api/routes', routes);
 
 // if(process.env.NODE_ENV === 'production'){
 //   app.use(express.static('frontend/build'));
 // }
 
-
-
-app.post("/answers", async (req, res) => {
-  const {QuizInfo} =req.body;
+app.post('/answers', async (req, res) => {
+  const { QuizInfo } = req.body;
 
   const data = {
-    QuizInfo:QuizInfo
+    QuizInfo: QuizInfo,
   };
-console.log(data);
-  try{
+  console.log(data);
+  try {
     await collection.insertMany([data]);
-  } catch(e)
-  {
-    res.json("fail");
+  } catch (e) {
+    res.json('fail');
   }
 });
 
 app.listen(8000, () => {
-  console.log("Port Connected!");
+  console.log('Port Connected!');
 });
