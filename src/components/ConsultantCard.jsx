29d6c3b75818daea17dcd3cd140d1c130656cc8b
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2023-06-10 10:46:41 +0530

    Add consult card

--- src/components/ConsultantCard.jsx
+++ src/components/ConsultantCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function ConsultantCard() {
-  return <div>ConsultantCard</div>;
-}
+const ConsultantCard = ({ photo, name, specialty, age }) => {
+  return (
+    <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white">
+      <img className=" w-full" src={photo} alt={name} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-gray-700 text-base">{specialty}</p>
+        <p className="text-gray-700 text-base">{age} years old</p>
+      </div>
+    </div>
+  );
+};
 
 export default ConsultantCard;
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Sidebar } from '../components';
 import ConsultantCard from '../components/ConsultantCard';
+import { usricon } from '../assets';
 function Book() {
+  const consultant = {
+    photo: usricon,
+    name: 'John Doe',
+    specialty: 'Psychologist',
+    age: 35,
+  };
+  console.log({ usricon });
   return (
     <>
       <div className="flex gap-10">
         <Sidebar />
-        BOOK A NEW APPOINTMENT?
-        <ConsultantCard />
+        <div className="flex flex-col gap-10">
+          <div>BOOK A NEW APPOINTMENT?</div>
+          <div className="container mx-auto flex-col flex-wrap">
+            <ConsultantCard {...consultant} />
+          </div>
+        </div>
       </div>
     </>
   );
