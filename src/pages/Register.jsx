86f43de810520eb6e811feedfc0461f2c3348e18
Lakshay <englakshaysk@gmail.com>
2023-06-10 19:09:04 +0530

    update home page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -50,29 +50,29 @@ function Register() {
                     <form action="POST">
                         <div className="flex flex-col items-center mt-4">
 
-                            <div className="m-4 w-1/2">
+                            <div className="m-2 w-1/2">
 
                                 <input type="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  onChange={(e) => { setName(e.target.value) }} placeholder="Name"  />
                             </div>
                             
-                            <div className="m-4 w-1/2">
+                            <div className="m-2 w-1/2">
 
                                 <input type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  onChange={(e) => { setEmail(e.target.value) }} placeholder="name@mail.com"  />
                             </div>
-                            <div className="m-4 w-1/2">
+                            <div className="m-2 w-1/2">
 
                                 <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  onChange={(e) => { setCity(e.target.value) }} placeholder="City"  />
                             </div>
 
-                            <div className="m-4 w-1/2">
+                            <div className="m-2 w-1/2">
 
                                 <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  onChange={(e) => { setCollege(e.target.value) }} placeholder="School/College/Institution"  />
                             </div>
 
-                            <div className="m-4 w-1/2">
+                            <div className="m-2 w-1/2">
                                 <input type="password" className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password"  />
                             </div>
-                            <div className="m-4">
+                            <div className="mt-4">
                                 <button  onClick={submit}  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                                     Register
                                 </button>
@@ -84,7 +84,7 @@ function Register() {
                         or
                     </div>
 
-                    <div className="mb-6 mt-3 hover:underline hover:text-blue-500">
+                    <div className="mb-6 mt-2 hover:underline hover:text-blue-500">
                         <Link to="/login">Already registered? Login</Link>
                     </div>
                 </div>
