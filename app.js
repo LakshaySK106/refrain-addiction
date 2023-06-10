3cd60d45034df5ebd4f33e085dd85813a869f71f
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2023-06-10 07:25:28 +0530

    add consultant login

--- app.js
+++ app.js
@@ -1,31 +1,88 @@
-const express = require("express");
-const collection = require("./mongo");
-const cors = require("cors");
+const express = require('express');
+const collection = require('./mongo');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+const mongoose = require('mongoose');
+const mongoString = 'mongodb://localhost:27017/refrain-addiction';
+mongoose.connect(
+  `mongodb+srv://refrain-addiction:codetogivehack@cluster0.yzcnusv.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+const database = mongoose.connection;
 
-app.get("/", cors(), (req, res) => {});
-
-app.post("/", async (req, res) => {
+database.on('error', (error) => {
+  console.log(error);
+});
+const consultantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  Age: {
+    type: Number,
+    required: true,
+  },
+  speciality: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const Consultant = mongoose.model('consultant', consultantSchema);
+app.get('/', cors(), (req, res) => {});
+app.get('/consultant', cors(), (req, res) => {});
+app.post('/', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const check = await collection.findOne({ email: email, password:password });
+    const check = await collection.findOne({
+      email: email,
+      password: password,
+    });
 
     if (check) {
-      res.json("exist");
+      res.json('exist');
     } else {
-      res.json("notexist");
+      res.json('notexist');
     }
   } catch (e) {
-    res.json("fail");
+    res.json('fail');
   }
 });
 
-app.post("/register", async (req, res) => {
-  const {name, email, city,college, password } = req.body;
+app.post('/consultant', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const check = await Consultant.findOne({
+      email: email,
+      password: password,
+    });
+
+    if (check) {
+      res.json('exist');
+    } else {
+      res.json('notexist');
+    }
+  } catch (e) {
+    res.json('fail');
+  }
+});
+app.post('/register', async (req, res) => {
+  const { name, email, city, college, password } = req.body;
 
   const data = {
     name: name,
@@ -39,16 +96,41 @@ app.post("/register", async (req, res) => {
     const check = await collection.findOne({ email: email });
 
     if (check) {
-      res.json("exist");
+      res.json('exist');
     } else {
-      res.json("notexist");
+      res.json('notexist');
       await collection.insertMany([data]);
     }
   } catch (e) {
-    res.json("fail");
+    res.json('fail');
+  }
+});
+app.post('/registerc', async (req, res) => {
+  const { name, email, speciality, password, age } = req.body;
+
+  const data = {
+    name: name,
+    email: email,
+    speciality: speciality,
+    password: password,
+    age: age,
+    isApproved: false,
+  };
+
+  try {
+    const check = await Consultant.findOne({ email: email });
+
+    if (check) {
+      res.json('exist');
+    } else {
+      res.json('notexist');
+      await Consultant.insertMany([data]);
+    }
+  } catch (e) {
+    res.json('fail');
   }
 });
 
 app.listen(8000, () => {
-  console.log("Port Connected!");
+  console.log('Port Connected!');
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom";
-import { Home, Assessment, Track, Chat, Profile, Landing, Book, Check, Login, Register } from './pages';
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {
+  Home,
+  Assessment,
+  Track,
+  Chat,
+  Profile,
+  Landing,
+  Book,
+  Check,
+  Login,
+  Register,
+} from './pages';
+import Consultant from './pages/Consultant';
+import Loginc from './pages/Loginc';
+import Registerc from './pages/Registerc';
 function App() {
   return (
     <div>
-       <Routes>
-          <Route exact path="/home" element={<Home />} />
-          <Route path="/assessment" element={<Assessment />} />
-          <Route path="/track" element={<Track />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/" element={<Landing />} />
-          <Route path="/Appointment/new" element={<Book />} />
-          <Route path="/Appointment/past" element={<Check />} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/login" element={<Login/>} />
-       </Routes>
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route path="/assessment" element={<Assessment />} />
+        <Route path="/track" element={<Track />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/consultant" element={<Consultant />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/Appointment/new" element={<Book />} />
+        <Route path="/Appointment/past" element={<Check />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registerc" element={<Registerc />} />
+        <Route path="/loginc" element={<Loginc />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
